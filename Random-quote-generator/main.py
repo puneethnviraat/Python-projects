9ef92cac99e576b16d85ca44bdd899f199def5c0
puneethnviraat <puneethnviraat@gmail.com>
2026-02-27 22:30:31 +0530

    Random-quote-generator

---
+++ Random-quote-generator/main.py
@@ -0,0 +1,16 @@
+import random
+
+quotes = [
+    "Believe you can and you're halfway there. – Theodore Roosevelt",
+    "Stay hungry. Stay foolish. – Steve Jobs",
+    "Be yourself; everyone else is already taken. – Oscar Wilde",
+    "Do or do not. There is no try. – Yoda",
+    "It always seems impossible until it's done. – Nelson Mandela",
+    "Knowladge,money,time. - jackie"
+    # ...add more quotes here!
+]
+
+print("💬 Welcome to the Quote Machine!")
+
+quote=random.choice(quotes)
+print("here is your quote :", quote)
